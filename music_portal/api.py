df7bc99c7eeac5ae946d15224f796a92e98c3de4
PUNEET-8 <puneetmakhija4@gmail.com>
2020-08-03 23:17:10 +0530

    Added Graph Section in Admin

--- music_portal/api.py
+++ music_portal/api.py
@@ -2,7 +2,9 @@ from modules.booking.models import Booking
 from django.db.models import Count
 from modules.teacher.models import Teacher
 from users.models import User
-
+from django.db.models import Count
+from modules.booking.models import Booking
+from django.db.models.functions import ExtractWeekDay
 from ninja import NinjaAPI
 import pdb
 
@@ -16,3 +18,29 @@ def dashboard_count(request):
 	student_count = User.objects.filter(is_superuser=False).count()
 	message = "Dashboard count fetched"
 	return {"message":message, "code": 200, "data": {"booking_count":booking_count, "teacher_count": teacher_count, "student_count":student_count}, 'error_message':""}
+
+@api.get("/dashboard_pie_chart")
+def booking_pie_chart_count(request):
+	query = Booking.objects.values('booking_status').annotate(count=Count('booking_status'))
+	total_count = [{'name': booking.get('booking_status'), 'y': booking.get('count')} for booking in query.iterator()]
+	return {"code": 200, "data": total_count}
+
+@api.get('/dashboard_line_chart')
+def booking_line_chart_count(request):
+	query = Booking.objects.annotate(weekday=ExtractWeekDay('created_at')).values('weekday').annotate(
+		count=Count('id')).values('weekday', 'count')
+	weeks = [0 for i in range(7)]
+	for booking in query.iterator():
+		weeks[booking.get('weekday')-1] = booking.get('count')
+	return {"code": 200, "data": weeks}
+
+@api.get('dashboard_bargraph')
+def booking_graph_view(request):
+	query = (Booking.objects.all().extra(
+		select={'month': "EXTRACT(month FROM created_at)", 'year': "EXTRACT(year FROM created_at)"}).values('month',
+																											'year').annotate(
+		count_items=Count('created_at'))).order_by('month')
+	months = [0 for i in range(12)]
+	for i in query.iterator():
+		months[int(i.get('month')) - 1] = i.get('count_items')
+	return {"code": 200, "data":months}
